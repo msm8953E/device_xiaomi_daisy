55086b33b70b25269f02673cfbc385be2393e3b3
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-03-05 23:01:43 +0600

    Being up

--- lineage_sakura.mk
+++ lineage_sakura.mk
@@ -14,11 +14,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 $(call inherit-product, device/xiaomi/sakura/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sakura
-PRODUCT_NAME := droidx_sakura
+PRODUCT_NAME := lineage_sakura
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 6 Pro
@@ -33,5 +33,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/sakura/sakura:9/PKQ1.180917.001/V12.0.1.0.PDICNXM:user/release-keys"
 
-DROIDX_BUILD_TYPE := OFFICIAL
-DROIDX_GAPPS := true
